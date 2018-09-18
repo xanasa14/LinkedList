3b7aa715b7c7aef1a3ec6466d9fc51e214e0466a
Xavier Navarro <xanasa14@hotmail.com>
2018-09-18 17:48:13 -0400

    Add files via upload
    Standard Created a Linked List using Python

---
+++ App.py
@@ -0,0 +1,11 @@
+from LinkedList import LinkedList;
+
+linkedList = LinkedList();
+linkedList.insertEnd("AS");
+linkedList.insertEnd("HOL");
+linkedList.insertEnd("bITCH");
+linkedList.insertStart("hI")
+linkedList.traverseList();
+linkedList.remove("HOL");
+linkedList.traverseList();
+print (linkedList.size())
